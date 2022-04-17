931de4c6afad2a2e1a439314cf32e40d9b07dea7
Hosea Kandie <zeddarn@gmail.com>
2022-04-16 21:11:27 -0500

    Cloudfront

--- deploy/create_cloudfront_cf.sh
+++ deploy/create_cloudfront_cf.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-aws s3 rm s3://rxpowet-bucket-02-logs --recursive
-
 aws cloudformation delete-stack \
-    --stack-name rx-powet-ecs-stack
+    --stack-name rx-powet-cloudfront-stack
 
 aws cloudformation wait stack-delete-complete \
-    --stack-name rx-powet-ecs-stack
+    --stack-name rx-powet-cloudfront-stack
 
-aws s3 cp ../aws/ecs-fargate.json s3://rxpowet-bucket-02/cf/ecs-fargate.json
+aws s3 cp ../aws/cloudfront-template.json s3://rxpowet-bucket-03/cf/cloudfront-template.json
 
 
 VPCID=`cat ./temp/vpcid`
@@ -17,20 +15,19 @@ SECURITYGROUP1=`cat ./temp/securitygroup1`
 SUBNETID01=`cat ./temp/subnetid01`
 SUBNETID02=`cat ./temp/subnetid02`
 HostedZones=`cat ./temp/HostedZones`
-SECRETARN=`cat ./temp/secretsarns`
 
-aws cloudformation create-stack --stack-name rx-powet-ecs-stack \
---template-url https://rxpowet-bucket-02.s3.amazonaws.com/cf/ecs-fargate.json \
+aws cloudformation create-stack --stack-name rx-powet-cloudfront-template-stack \
+--template-url https://rxpowet-bucket-03.s3.amazonaws.com/cf/cloudfront-template.json \
 --capabilities CAPABILITY_NAMED_IAM \
 --on-failure DO_NOTHING \
 --parameters \
-ParameterKey=AppVersion,ParameterValue=0.0.3 \
 ParameterKey=VPC,ParameterValue=${VPCID} \
 ParameterKey=HostedZoneName,ParameterValue=${HostedZones} \
 ParameterKey=SubnetA,ParameterValue=${SUBNETID01} \
-ParameterKey=SECRETARN,ParameterValue=${SECRETARN} \
 ParameterKey=SubnetB,ParameterValue=${SUBNETID02} \
 ParameterKey=ACCOUNTID,ParameterValue=${ACCOUNTID}
 
 aws cloudformation wait stack-create-complete \
-    --stack-name rx-powet-ecs-stack
+    --stack-name rx-powet-cloudfront-template-stack
+
+aws s3 cp ../html/dashboard s3://rxpowet-bucket-front/   
